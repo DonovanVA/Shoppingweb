a21f4e6412c19de18fc2fcc33380ed28fb17e60a
DonovanVA <donovanipman@gmail.com>
2022-01-10 01:10:59 +0800

    v 1.0.1

--- src/Components/Tcart.js
+++ src/Components/Tcart.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 import './Tcart.css'
+import { useEffect } from 'react';
+import { useHistory } from 'react-router';
 function Tcart({ Cart, setCart,Logged,setLoggedin,Price,setPrice }) {
   
   const getTotalSum = () => {
